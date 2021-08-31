0ddfed6157b7402971c5e2e301b5c75babffd8e7
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-08-30 20:13:08 -0500

    tipos de datos, conversiones y concatenación

--- clase1.py
+++ clase1.py
@@ -5,3 +5,63 @@ Created on Mon Aug 30 19:29:51 2021
 @author: Usuario
 """
 
+print('hello world')
+
+# Variables
+a = 3
+print(type(a))
+
+# Suma
+a = 2
+b = 5
+c = a + b
+print(c)
+
+# Resta
+a = 2
+b = 5
+c = a - b
+print(c)
+
+# Potencia
+a = 2
+b = 5
+c = a**b
+print(c)
+
+
+# Tipos de datos
+# String str
+a = "hola mundo"
+a = 'hola mundo'
+b = " I can't do it"
+c = 'Alias "Roberto"'
+
+# Entero int
+a = 5
+
+# Decimal float
+a = 5.6
+
+# Booleano bool
+x = True
+y = False
+
+# Conversiones
+a = '3'
+y = int(a)
+print(y)
+print(type(y))
+
+a = 3
+y = str(a)
+print(y)
+print(type(y))
+
+# Concatenaciones
+a = "hola"
+b = "mundo"
+c = a + " " + b
+
+a = "hola"
+b = a*5
