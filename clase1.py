e7a35f031a80c0ee23f514cab049ed4d4b5de083
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-08-30 20:55:16 -0500

    Captura por pantalla y ejercicios condicionales

--- clase1.py
+++ clase1.py
@@ -65,3 +65,44 @@ c = a + " " + b
 
 a = "hola"
 b = a*5
+
+# Captura por pantalla
+nombre = input('Digite su nomnbre: ')
+print('Hola', nombre)
+
+print('Digite su nombre: ')
+nombre = input()
+print('Hola', nombre)
+
+# HUA que sume dos numeros e imprima su resultado
+num = input('Digite el primer número a sumar: ')
+num2 = input('Digite el segundo número a sumar: ')
+resul = int(num) + int(num2)
+print("El resultado de la suma es: ", resul)
+
+num = float(input('Digite el primer número a sumar: '))
+num2 = float(input('Digite el segundo número a sumar: '))
+resul = num + num2
+# print("El resultado de la suma es: ", resul)
+print(f'La suma de lso números {num} + {num2} es {resul}')
+
+# Lea un número y lo eleve al cuadrado
+numero = float(input('Digite el número a elevar al cuadrado: '))
+resultado = numero**2
+print(f'El número {numero} elevado al cuadrado es igual a: {resultado}')
+
+# Tomar el valor de un producto, le aplique el 20% de un descuento, imprima el
+# valor del producto inicial, el valor con desceunto y el valor ahorrado
+valor_producto = float(input('Digite el valor del producto: '))
+descuento = valor_producto*0.20
+valor_total = valor_producto - descuento
+print(f'El valor inicial del producto es: ${valor_producto:,}')
+print(f'El valor del descuento es: ${descuento:,}')
+print(f'El valor final del producto es: ${valor_total:,}')
+
+vproduct = float(input('Digite el valor del producto: $'))
+descuent = vproduct * 0.20
+vfinal = vproduct - descuent
+print(f'El valor inicial del producto es: ${vproduct:,}')
+print(f'El valor del descuento es: ${descuent:,}')
+print(f'El valor final del producto es: ${vfinal:,}')
