897dfb238f09b8e0760409f5af0cfc8f1ca3a039
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-09-06 19:25:12 -0500

    Tabla de verdad, Jerarquía de operaciones y condiciones

--- clase2.py
+++ clase2.py
@@ -5,3 +5,27 @@ Created on Mon Sep  6 19:02:07 2021
 @author: Usuario
 """
 
+# Condicionales
+# Tabla de verdad
+
+# Tabla del and
+# v and v = v
+# v and f = f
+# f and v = f
+# f and f = f
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Table del or
+# v or v = v
+# v or f = v
+# f or v = v
+# f or f = f
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
